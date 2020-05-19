7518d1b515164c8a86c0fc4e67cb2c7835e405e7
Diego <dariag00@estudiantes.unileon.es>
2020-05-19 17:36:50 +0200

    First implementation of tablet layout

--- app/src/main/java/com/example/bakingapp/ui/recipe/RecipeActivity.java
+++ app/src/main/java/com/example/bakingapp/ui/recipe/RecipeActivity.java
@@ -5,16 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.example.bakingapp.R;
 import com.example.bakingapp.data.http.recipes.Recipe;
+import com.example.bakingapp.ui.detail.StepDetailFragment;
+
+import butterknife.BindView;
 
 import static com.example.bakingapp.ui.recipe.RecipeFragment.RECIPEE;
 
 public class RecipeActivity extends AppCompatActivity {
 
     private Recipe recipe;
+    private boolean twoPane;
+    @BindView(R.id.recipee_steps_container)
+    FrameLayout stepsRecipeContainer;
+    private int currentStep;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +39,20 @@ public class RecipeActivity extends AppCompatActivity {
         Bundle bundle = intent.getExtras();
         if(bundle != null){
             recipe = (Recipe) bundle.getSerializable("recipe");
+        }
+
+        if(stepsRecipeContainer != null){
+            twoPane = true;
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            currentStep = 0;
+            StepDetailFragment stepFragment = new StepDetailFragment();
+            stepFragment.setStep(recipe.getSteps().get(currentStep));
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.recipee_steps_container, stepFragment)
+                    .commit();
+        } else {
+            twoPane = false;
         }
 
         if(recipe != null){
